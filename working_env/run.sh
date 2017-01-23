29ea76e63b018b61dd5841bcbc94e5c20f235e42
dyson0913 <hhg4092@gmail.com>
2017-01-23 15:35:15 +0000

    open port

--- working_env/run.sh
+++ working_env/run.sh
@@ -1,2 +1,2 @@
-sudo docker run -i -t -v /home/dyson/server/:/server -p 7000:7000 --name="server" --link redis:redis workshop:dyson
+sudo docker run -i -t -v /home/dyson/server/:/server -p 7000:7000 -p 6007:6007 -p 6011:6011 -p 6029:6029 -p 6037:6037 -p 6043:6043 -p 6047:6047 -p 6049:6049 -p 6053:6053 -p 6067:6067 --name="server" workshop:dyson
  

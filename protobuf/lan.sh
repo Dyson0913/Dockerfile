565878b52b2c4aa562c96f9e12c2459dddeac20a
Dyson <hhg4092@gmail.com>
2017-12-29 20:05:21 +0700

    proto3

---
+++ protobuf/lan.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/protoc --python_out=\. System.proto Landlords.proto threecard.proto ox100.proto
---
+++ protobuf/macRun.sh
@@ -0,0 +1,2 @@
+sudo docker run -i -t -v /Users/user3/work/Dockerfile/protobuf/:/test  --name="proto" proto:dyson
+ 

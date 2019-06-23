52e85685765ab47cef0c0c519d940d49f3f95ccb
Dyson0913 <hhg4092@gmail.com>
2019-06-23 09:29:58 +0000

    ftp & jp server

--- nginx/ftp.sh
+++ nginx/ftp.sh
@@ -1,1 +1,1 @@
-docker run -d -v $(pwd)/data/:/home/vsftpd -p 20:20 -p 21:21 -p 47400-47470:47400-47470 -e FTP_USER=xxx -e FTP_PASS=xxx -e PASV_ADDRESS=ip --name ftp --restart=always bogem/ftp
+docker run -d -v $(pwd)/data/:/home/vsftpd -p 20:20 -p 21:21 -p 47400-47470:47400-47470 -e FTP_USER=dyson -e FTP_PASS=dyson -e PASV_ADDRESS=207.148.90.187 --name ftp --restart=always bogem/ftp
---
+++ working_env/jprun.sh
@@ -0,0 +1,2 @@
+sudo docker run -i -t -v /root/project/pyserver:/server -p 7000:7000 -p 6007:6007 -p 6011:6011 -p 6029:6029 -p 6037:6037 -p 6043:6043 -p 6047:6047 -p 6049:6049 -p 6053:6053 -p 6067:6067 --name="server" workshop:dyson
+ 

f0ad1192d1cfd218f0a0bf83d20525ac641d6778
dyson0913 <hhg4092@gmail.com>
2019-12-19 01:12:44 +0800

    dbproxy

---
+++ dbproxy/build.sh
@@ -0,0 +1,2 @@
+cd ../../
+docker build -t="dbproxy:dyson" -f Dockerfile/dbproxy/Dockerfile .
---
+++ dbproxy/run.sh
@@ -0,0 +1,3 @@
+cd ../../ 
+sudo docker run -i -t -v $(pwd)/dbproxy:/home --name="dbproxy" dbproxy:dyson
+ 

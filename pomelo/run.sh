a1a918ac99cb1324a3a3b050c2af20d1e976ebab
dyson <hhg4092@gmail.com>
2020-03-05 10:48:34 +0800

    pomelo fix

--- pomelo/run.sh
+++ pomelo/run.sh
@@ -1,3 +1,3 @@
 cd ../../
-docker run -i -t -v $(pwd)/pomelo:/home -p 3001:3001 -p 3010:3010 --name="pomelo" pomelo:dyson
+docker run -i -t -v $(pwd)/pomelo:/home -p 3001:3001 -p 3010:3010 -p 3014:3014 -p 3051:3051 -p 3052:3052 --name="pomelo" pomelo:dyson
  

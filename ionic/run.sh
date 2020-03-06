dd650b5d680c5079994db58a5aa444fe12122047
dyson <hhg4092@gmail.com>
2020-03-06 17:58:52 +0800

    flask fix

--- ionic/run.sh
+++ ionic/run.sh
@@ -1,3 +1,3 @@
 cd ../../
-docker run -i -t -v $(pwd)/godpointclient/turto:/home -p 8100:8100 -p 35729:35729 --name="ionic" ionic:dyson
+docker run -i -t -v $(pwd)/ionic:/home -p 8100:8100 -p 35729:35729 --name="ionic" ionic:dyson
  

bd1ca6e4d2f2afa4a6fb9bfe8aa000c3ee762c97
dyson0913 <hhg4092@gmail.com>
2020-03-08 12:18:10 +0800

    one cont

--- working_env/connector.sh
+++ working_env/connector.sh
@@ -1,2 +1,2 @@
 cd ../../
-sudo docker run -i -t -v $(pwd)/pyserver:/home --name="connector" --net pynet workshop:dyson
+sudo docker run -i -t -v $(pwd)/pyserver:/home -p 6007:6007 -p 6011:6011 -p 6029:6029 -p 6043:6043 -p 6037:6037 -p 6047:6047 --name="pyserver" --net pynet workshop:dyson

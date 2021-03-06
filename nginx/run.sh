75570d5971a77ed591152824772ebb40c3d89097
dyson4092 <hhg4092@gmail.com>
2021-03-06 19:51:33 +0000

    nginx websocket轉發

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,5 +1,5 @@
 cd ../../
-sudo docker run -i -t -p 80:80 \
+sudo docker run -i -t -p 80:80 -p 8090:8092 --add-host=localhost:140.82.57.250 \
 -v $(pwd)/nginx:/home/data \
 -v $(pwd)/Dockerfile/nginx/httpconf/:/home \
 --name="nginx" nginx:dyson 

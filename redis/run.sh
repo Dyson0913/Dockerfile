730d7da4dcdd5dfa676d1602b526b554c72bdaf1
dyson4092 <hhg4092@gmail.com>
2021-05-08 18:45:09 +0400

    redis_command fix

--- redis/run.sh
+++ redis/run.sh
@@ -1,2 +1,2 @@
-sudo docker run -i -t -p 6379:6379 --name="redis" --net py-net redis:dyson 
+sudo docker run -i -t -p 6379:6379 --name="redis"  redis:dyson 
  
--- redis_Commander/run.sh
+++ redis_Commander/run.sh
@@ -1,2 +1,2 @@
-docker run -i -t -p 8081:8081 --name="rediscommander" --net py-net rediscommander:dyson
+docker run -i -t  --network host --name="rediscommander"  rediscommander:dyson
  
